3aea148a1d9de5b31adb0450b4071e2ab7adfe2d
lh000920 <1260394142@qq.com>
2022-03-30 18:00:03 +0800

    Session and Cookies

--- src/main/java/lh/sustech/community/community/controller/AuthorizeController.java
+++ src/main/java/lh/sustech/community/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -26,7 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                            @RequestParam(name="state") String state){
+                            @RequestParam(name="state") String state,
+                            HttpServletRequest request){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -36,7 +39,16 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+
+        if(user!=null){
+            //login successfully
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }
+        else{
+            //login unsuccessfully
+            return "redirect:/";
+        }
+
     }
 }
--- src/main/java/lh/sustech/community/community/provider/GithubProvider.java
+++ src/main/java/lh/sustech/community/community/provider/GithubProvider.java
@@ -21,7 +21,6 @@ public class GithubProvider {
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String string = response.body().string();
-            System.out.println(string);
             String[] split = string.split("&");
             String tokenStr = split[0];
             String[] tokenSubStr = tokenStr.split("=");
