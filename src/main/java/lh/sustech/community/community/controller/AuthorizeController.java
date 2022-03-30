b3e73bdd5e2b68d0fcafadcda3ce38915aa82ee1
lh000920 <1260394142@qq.com>
2022-03-30 13:05:24 +0800

    change app.pro

--- src/main/java/lh/sustech/community/community/controller/AuthorizeController.java
+++ src/main/java/lh/sustech/community/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import lh.sustech.community.community.dto.AccessTokenDTO;
 import lh.sustech.community.community.dto.GithubUser;
 import lh.sustech.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,16 +15,25 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                             @RequestParam(name="state") String state){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id("8e7dd234214b4ce11362");
-        accessTokenDTO.setClient_secret("d0da41ce0af3724fe9b7f1c87d3f49a57e3136f0");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
